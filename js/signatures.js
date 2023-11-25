98c2b46453797f214de107a7cb1d5f86d8b7d650
jhawkreynolds <151973630+teammartha@users.noreply.github.com>
2023-11-24 20:32:18 -0600

    Adding signatures

--- js/signatures.js
+++
@@ -1,6 +0,0 @@
-function signaturePasta(){
-    return "Pasta (he/they) 🏳️‍🌈&#10;Lead Developer&#10;pokeheartgold-expanded";
-    //Pasta (he/they) 🏳️‍🌈
-    //Lead Developer
-    //pokeheartgold-expanded
-}
